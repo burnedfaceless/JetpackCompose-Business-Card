65a18b1393894735a639823a554a866d828f87ba
Brian Abbott <burnedfaceless@gmail.com>
2025-11-11 14:18:13 -0500

    continued building out ui with compose

--- app/src/main/java/com/brianabbott/jetpackcompose_business_card/MainActivity.kt
+++ app/src/main/java/com/brianabbott/jetpackcompose_business_card/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -21,6 +25,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.brianabbott.jetpackcompose_business_card.ui.theme.JetpackComposeBusinessCardTheme
@@ -48,8 +53,19 @@ fun CreateBusinessCard() {
             shape = RoundedCornerShape(corner = CornerSize(15.dp)),
             CardDefaults.cardColors(containerColor = Color.White),
             elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)) {
-
         }
+
+        Surface(modifier = Modifier.size(150.dp)
+            .padding(5.dp),
+            shape = CircleShape,
+            border = BorderStroke(0.5.dp, Color.LightGray),
+            shadowElevation = 4.dp,
+            color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)) {
+            Image(painter = painterResource(id = R.drawable.profile_image),
+                contentDescription = "profile image",
+                modifier = Modifier.size(135.dp))
+        }
+
     }
 }
 
